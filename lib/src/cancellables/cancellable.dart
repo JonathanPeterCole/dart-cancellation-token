9a2afa51a80d4a1e91e59367ad0430f97ef1b980
Jonathan Cole <JonathanPeterCole@gmail.com>
2022-01-18 21:17:04 +0000

    Add doc comment and @visibleForOverriding to onCancel method

--- lib/src/cancellables/cancellable.dart
+++ lib/src/cancellables/cancellable.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 /// A mixin for making a class cancellable using a [CancellationToken].
 ///
 /// This does not handle attaching to and detatching from the token.
@@ -5,5 +7,10 @@
 /// Classes using this mixin should call `isCancelled` and `attach(this)` on the token when they're
 /// created, and call `detatch(this)` on the token once complete to prevent memory leaks.
 mixin Cancellable {
+  /// Called when the attached token is cancelled.
+  ///
+  /// It's not necessary to detach from the token in this method, as cancellation tokens detach from
+  /// all cancellables when cancelled.
+  @visibleForOverriding
   void onCancel(Exception cancelException);
 }
